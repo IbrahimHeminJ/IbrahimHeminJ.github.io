a27cc8cd353fcd3f4d361ff95bc6a6eda010eb5f
IbrahimHeminJ <ibhj.2304@gmail.com>
2025-09-18 16:50:12 +0300

    initial commit for my vuepress profile

--- vuepress-starter/docs/.vuepress/.temp/git/config.js
+++ vuepress-starter/docs/.vuepress/.temp/git/config.js
@@ -1,5 +1,5 @@
-import { GitContributors } from "D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
-import { GitChangelog } from "D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/node_modules/@vuepress/plugin-git/lib/client/components/GitChangelog.js";
+import { GitContributors } from "D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-git@2.0.0-_5feb42c87d984990e55a10f67e71fcfd/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
+import { GitChangelog } from "D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-git@2.0.0-_5feb42c87d984990e55a10f67e71fcfd/node_modules/@vuepress/plugin-git/lib/client/components/GitChangelog.js";
 
 export default {
   enhance: ({ app }) => {
--- vuepress-starter/docs/.vuepress/.temp/internal/clientConfigs.js
+++ vuepress-starter/docs/.vuepress/.temp/internal/clientConfigs.js
@@ -1,14 +1,14 @@
-import * as clientConfig0 from 'D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/node_modules/@vuepress/plugin-active-header-links/lib/client/config.js'
-import * as clientConfig1 from 'D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/node_modules/@vuepress/plugin-back-to-top/lib/client/config.js'
-import * as clientConfig2 from 'D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/node_modules/@vuepress/plugin-copy-code/lib/client/config.js'
-import * as clientConfig3 from 'D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/node_modules/@vuepress/plugin-markdown-hint/lib/client/config.js'
+import * as clientConfig0 from 'D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-active-hea_8e38e4e6e02e09c98668c6f23ecfff2c/node_modules/@vuepress/plugin-active-header-links/lib/client/config.js'
+import * as clientConfig1 from 'D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-back-to-to_733c720fbc6be9d0dd7655eeed2817c2/node_modules/@vuepress/plugin-back-to-top/lib/client/config.js'
+import * as clientConfig2 from 'D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-copy-code@_d1d5a9e5a8d6f44ff1ea40d28bc62674/node_modules/@vuepress/plugin-copy-code/lib/client/config.js'
+import * as clientConfig3 from 'D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-markdown-h_a035151eadc9ce8c15d154c1716db786/node_modules/@vuepress/plugin-markdown-hint/lib/client/config.js'
 import * as clientConfig4 from 'D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/docs/.vuepress/.temp/git/config.js'
-import * as clientConfig5 from 'D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/node_modules/@vuepress/plugin-medium-zoom/lib/client/config.js'
-import * as clientConfig6 from 'D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/node_modules/@vuepress/plugin-nprogress/lib/client/config.js'
+import * as clientConfig5 from 'D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-medium-zoo_af24d0e478e02601a8eac9dbbea65e9d/node_modules/@vuepress/plugin-medium-zoom/lib/client/config.js'
+import * as clientConfig6 from 'D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-nprogress@_e5ef4a5b676a358e8115451458c97c70/node_modules/@vuepress/plugin-nprogress/lib/client/config.js'
 import * as clientConfig7 from 'D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/docs/.vuepress/.temp/prismjs/config.js'
 import * as clientConfig8 from 'D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/docs/.vuepress/.temp/markdown-tab/config.js'
-import * as clientConfig9 from 'D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/node_modules/@vuepress/plugin-theme-data/lib/client/config.js'
-import * as clientConfig10 from 'D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/node_modules/@vuepress/theme-default/lib/client/config.js'
+import * as clientConfig9 from 'D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-theme-data_f7f4cbf3268f81a41dd39d0c827f134f/node_modules/@vuepress/plugin-theme-data/lib/client/config.js'
+import * as clientConfig10 from 'D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/node_modules/.pnpm/@vuepress+theme-default@2.0_d12ce619a66c52d41138d1d093c8734b/node_modules/@vuepress/theme-default/lib/client/config.js'
 
 export const clientConfigs = [
   clientConfig0,
