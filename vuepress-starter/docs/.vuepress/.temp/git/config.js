b7d2a679263436c8e01c742dc527512c1bdf6711
IbrahimHeminJ <ibhj.2304@gmail.com>
2025-09-18 17:07:58 +0300

    Delete vuepress-starter directory
    new start

--- vuepress-starter/docs/.vuepress/.temp/git/config.js
+++
@@ -1,9 +0,0 @@
-import { GitContributors } from "D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-git@2.0.0-_5feb42c87d984990e55a10f67e71fcfd/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
-import { GitChangelog } from "D:/IBRAHIM/College junk/Fouth Grade/Web Appliaction/project/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-git@2.0.0-_5feb42c87d984990e55a10f67e71fcfd/node_modules/@vuepress/plugin-git/lib/client/components/GitChangelog.js";
-
-export default {
-  enhance: ({ app }) => {
-    app.component("GitContributors", GitContributors);
-    app.component("GitChangelog", GitChangelog);
-  },
-};
